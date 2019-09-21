abf50692df30a15c4374fb58bc0817bbb658f65b
avinash8142 <48849618+avinash8142@users.noreply.github.com>
2019-09-21 13:41:32 +0530

    LW upload

---
+++ LWRestServices/src/main/java/com/lfg/nb/apps/lw/LwRestServicesApplication.java
@@ -0,0 +1,13 @@
+package com.lfg.nb.apps.lw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LwRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LwRestServicesApplication.class, args);
+	}
+
+}
